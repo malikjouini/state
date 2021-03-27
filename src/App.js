3246fbf1d376c5faecf9f7a22dd576fbec2e48bf
malikjouini <malekjouini47@gmail.com>
2021-03-27 11:13:57 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import myImage from './ka7la.png'
+import './App.css'
+export default class App extends React.Component {
+  state = {
+    Person: {
+      fullName: "Malek Jouini",
+      bio: "Hello go my code",
+      imgSrc: myImage,
+      profession: "developer web",
+    },
+    show: true,
+    date:""
+  };
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  toggleButton = () => {
+    this.setState({
+      ...this.state,
+      show: !this.state.show
+    });
+  };
+  componentDidMount(){
+ 
+   setInterval(  ()=>{
+     let newdate = new Date()
+   newdate = newdate.getHours()+ ':' + newdate.getMinutes()
+   this.setState({date:newdate})
+  },1000)
 }
+  render() {
+    return (
+      <div className="box">
+        {this.state.show && (
+          <>
+            <h2>{this.state.Person.fullName}</h2>
+            <h2>{this.state.Person.bio}</h2>
+            <img src={this.state.Person.imgSrc}></img>
+            <h2>{this.state.Person.profession}</h2>
+            <h2>{this.state.date}</h2>
+            <br></br>
+          </>
+        )}
 
-export default App;
+        <button onClick={this.toggleButton}>click here</button>
+      </div>
+    );
+  }
+}
